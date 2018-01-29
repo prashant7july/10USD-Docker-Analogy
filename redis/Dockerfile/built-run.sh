6eb0cb8c49e08bf5a819d9fa16600a3596bb0f7f
Prashant Shekher <prashant.shekher@wishfin.com>
2018-01-29 11:55:13 +0530

    Modify Sell Script

--- redis/Dockerfile/built-run.sh
+++ redis/Dockerfile/built-run.sh
@@ -2,4 +2,6 @@
 # Build Redis Images
 docker build -t redis-server .
 # Run a Redis-Server Instance
-docker run --name redisinstance -t redis-server
+#docker run --name redisinstance -t redis-server
+#docker run --rm -it -p 0.0.0.0:6379:6379 --name redis redis:alpine [FOR DOCKER HUB]
+docker run --rm -it -p 0.0.0.0:6379:6379 --name redisinstance redis-server
