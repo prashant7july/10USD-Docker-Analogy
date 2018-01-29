3e624746015d361a1d93aae3e635126c2fb784d2
Prashant Shekher <prashant.shekher@wishfin.com>
2018-01-29 12:54:52 +0530

    modify code

--- redis/Dockerfile2ndstcase/built-run.sh
+++ redis/Dockerfile2ndstcase/built-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# chmod +x built-run.sh
+
 # Build Redis Images
 docker build -t redis-server .
 # Run a Redis-Server Instance
-#docker run --name redisinstance -t redis-server
+docker run --name redisinstance -t redis-server
 #docker run --rm -it -p 0.0.0.0:6379:6379 --name redis redis:alpine [FOR DOCKER HUB]
-docker run --rm -it -p 0.0.0.0:6379:6379 --name redisinstance redis-server
+#docker run --rm -it -p 0.0.0.0:6379:6379 --name redisinstance redis-server
